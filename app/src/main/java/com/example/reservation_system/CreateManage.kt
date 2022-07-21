331e893a55a9881201c4feb4e48eb9fc655a6fac
Jongwon-0518 <77ha@naver.com>
2022-07-22 00:34:16 +0900

    .

--- app/src/main/java/com/example/reservation_system/CreateManage.kt
+++ app/src/main/java/com/example/reservation_system/CreateManage.kt
@@ -10,7 +10,8 @@ import androidx.fragment.app.Fragment
 class CreateManage : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_createmanage, container, false)
+        var rootview = inflater.inflate(R.layout.fragment_createmanage, container, false)
+        return rootview
     }
 
 }
