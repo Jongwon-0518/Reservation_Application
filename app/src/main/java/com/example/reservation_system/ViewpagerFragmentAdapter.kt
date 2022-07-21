a08ce5429b785f059e877eb82e0d6b35e7cbf3f5
Jongwon-0518 <77ha@naver.com>
2022-07-21 17:03:01 +0900

    test

--- app/src/main/java/com/example/reservation_system/ViewpagerFragmentAdapter.kt
+++ app/src/main/java/com/example/reservation_system/ViewpagerFragmentAdapter.kt
@@ -47,6 +47,7 @@ class Reservations : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_reservations, container, false)
+        println("종짱")
     }
 
 }
