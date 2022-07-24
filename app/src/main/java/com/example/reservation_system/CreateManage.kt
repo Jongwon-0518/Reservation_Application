1f76d6990f61e035f70f3ade0e4b9074f151917b
Jongwon-0518 <77ha@naver.com>
2022-07-24 15:48:14 +0900

    mypage

--- app/src/main/java/com/example/reservation_system/CreateManage.kt
+++ app/src/main/java/com/example/reservation_system/CreateManage.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.example.reservation_system.databinding.FragmentHomeBinding
 import kotlinx.android.synthetic.main.fragment_createmanage.*
 
 
