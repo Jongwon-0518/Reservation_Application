bbef9307efe4264cc5bfb57c758b37d1d3467af0
Jongwon-0518 <77ha@naver.com>
2022-07-25 22:10:56 +0900

    makeroom

--- app/src/main/java/com/example/reservation_system/CreateManage.kt
+++ app/src/main/java/com/example/reservation_system/CreateManage.kt
@@ -14,6 +14,7 @@ class CreateManage : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_createmanage, container, false)
+
         make_room_button.setOnClickListener {
             activity?.let {
                 val intent = Intent(context, Makeroom::class.java)
