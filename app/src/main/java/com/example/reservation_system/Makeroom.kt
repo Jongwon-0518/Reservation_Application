478dbe5854e41e72326efbdc4a88a5028db3adca
Jongwon-0518 <77ha@naver.com>
2022-07-26 19:42:51 +0900

    map

--- app/src/main/java/com/example/reservation_system/Makeroom.kt
+++ app/src/main/java/com/example/reservation_system/Makeroom.kt
@@ -2,16 +2,25 @@ package com.example.reservation_system
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.make_room.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.MarkerOptions
 
-class Makeroom : AppCompatActivity() {
+class Makeroom : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.make_room)
+
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         Make_room_complete.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
@@ -19,6 +28,15 @@ class Makeroom : AppCompatActivity() {
             this.finish()
         }
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val marker = LatLng(35.241615, 128.695587)
+        mMap.addMarker(MarkerOptions().position(marker).title("마커 제목"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+    }
+
+
 
     override fun finish() {
         super.finish()
