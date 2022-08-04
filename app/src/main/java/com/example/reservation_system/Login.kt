6cda1cba3a8d21559690dbeddaa3f2e201cd4f38
Leeseunghun <leeseunghun98@naver.com>
2022-08-04 20:19:37 +0900

    Update Login

--- app/src/main/java/com/example/reservation_system/Login.kt
+++ app/src/main/java/com/example/reservation_system/Login.kt
@@ -1,8 +1,14 @@
 package com.example.reservation_system
 
+import android.content.Context
 import android.content.Intent
+import android.hardware.input.InputManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
@@ -10,14 +16,56 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        button_login.setOnClickListener{
-            val id = editText_userid.text.toString()
-            val pw = editText_userpassword.text.toString()
+        button_login.setOnClickListener {
+            Log_in(it)
+        }
 
-            if (id == "1111" && pw == "1111") {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+        editText_userpassword.setOnEditorActionListener{ v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                Log_in(v)
+                true
             }
+            else false
+        }
+
+        // 불러오기
+        var pref = this.getPreferences(0)
+        editText_userid.setText(pref.getString("ID", ""))
+        editText_userpassword.setText(pref.getString("PW", ""))
+        checkbox_saveID.isChecked = pref.getBoolean("Cb_Save", false)
+
+        val logged_out = intent.getBooleanExtra("From_Logout", false)
+        // 로그아웃이 진행된 경우 자동로그인 해제
+        if (!logged_out){
+            checkbox_autoLogin.isChecked = pref.getBoolean("Cb_Autologin", false)
+        }
+        // 자동로그인 진행
+        if (checkbox_autoLogin.isChecked){
+            Log_in(null)
+        }
+    }
+
+    private fun Log_in(v : View?){
+        // 키보드 숨기기
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(v?.windowToken, 0)
+
+        if (editText_userid.text.toString() == "1111" && editText_userpassword.text.toString() == "1111"){
+            Toast.makeText(this, "OOO님 환영합니다!", Toast.LENGTH_SHORT).show()
+
+            // checkbox 저장
+            this.getPreferences(0).edit().putBoolean("Cb_Autologin", checkbox_autoLogin.isChecked).apply()
+            this.getPreferences(0).edit().putBoolean("Cb_Save", checkbox_saveID.isChecked).apply()
+
+            // 저장
+            if (checkbox_saveID.isChecked){
+                this.getPreferences(0).edit().putString("ID", editText_userid.text.toString()).apply()
+                this.getPreferences(0).edit().putString("PW", editText_userpassword.text.toString()).apply()
+            }
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("ID", "아이디")
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/reservation_system/Mypage.kt
+++ app/src/main/java/com/example/reservation_system/Mypage.kt
@@ -1,16 +1,27 @@
 package com.example.reservation_system
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_mypage.view.*
 
 
 class Mypage : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
-    }
+        val rootView = inflater.inflate(R.layout.fragment_mypage, container, false)
 
+        rootView.button_logout.setOnClickListener{
+            val intent = Intent(context, Login::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.putExtra("From_Logout", true)
+            startActivity(intent)
+        }
+
+        return rootView
+    }
 }
