48a01ca7c0121414a45fdd62a10391fd4bcc59c1
Leeseunghun <leeseunghun98@naver.com>
2022-08-21 17:44:22 +0900

    update CreateManage (showing my rooms)

--- app/src/main/java/com/example/reservation_system/CreateManage.kt
+++ app/src/main/java/com/example/reservation_system/CreateManage.kt
@@ -34,8 +34,11 @@ class CreateManage : Fragment() {
 //        room_Data("010-XXXX-XXXX", "3번방", "3번방 이에요. 3번방 이에요. 3번방 이에요. 3번방 이에요. 3번방 이에요. 3번방 이에요.", 3, "health")
 //    )
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_createmanage, container, false)
+        val DataList = arrayListOf<room_Data>()
+        val adapter = HomeRecyclerViewAdapter(DataList)
 
         database = Firebase.database.reference
 
@@ -44,13 +47,31 @@ class CreateManage : Fragment() {
         // 구분선
         make_room_recylerView.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
         make_room_recylerView.layoutManager = LinearLayoutManager(requireContext())
-        make_room_recylerView.adapter = HomeRecyclerViewAdapter()
+        make_room_recylerView.adapter = adapter
         rootView.make_room_button.setOnClickListener{
             activity?.let {
                 val intent = Intent(context, Makeroom::class.java)
                 startActivity(intent)
                 requireActivity().overridePendingTransition(R.anim.slide_right_enter, R.anim.slide_right_exit)
             }
+        }
+
+        // Read Database
+        database.child("User").child(getUserPhoneNumber()).child("makeroom_codes").get().addOnSuccessListener { it ->
+            val room_lists = it.value as ArrayList<*>
+            room_lists.forEach { a ->
+                a?.run{
+                    database.child("Room").child(a as String).get().addOnSuccessListener {
+                        val map = it.value as HashMap<*, *>
+                        DataList.add(room_Data(map["maker"] as String, map["title"] as String, map["information"] as String, (map["code"] as Long).toInt(), map["room_category"] as String))
+                        adapter.notifyDataSetChanged()
+                    }.addOnFailureListener{
+                        Log.e("firebase", "Error getting data", it)
+                    }
+                }
+            }
+        }.addOnFailureListener{
+            Log.e("firebase", "Error getting data", it)
         }
 
         return rootView
@@ -77,34 +98,8 @@ class CreateManage : Fragment() {
         }
     }
 
-// TODO : NotifyDataSetChanged
-
     @SuppressLint("NotifyDataSetChanged")
-    inner class HomeRecyclerViewAdapter() : RecyclerView.Adapter<HomeRecyclerViewHolder>() {
-
-        val room_data_list : ArrayList<room_Data> = arrayListOf()
-
-        init {
-            // Read Database
-            val DataList = arrayListOf<room_Data>()
-            database.child("User").child(getUserPhoneNumber()).child("makeroom_codes").get().addOnSuccessListener { it ->
-                val room_lists = it.value as ArrayList<*>
-                room_lists.forEach { a ->
-                    a?.run{
-                        database.child("Room").child(a as String).get().addOnSuccessListener {
-                            val map = it.value as HashMap<*, *>
-                            DataList.add(room_Data(map["maker"] as String, map["title"] as String, map["information"] as String, (map["code"] as Long).toInt(), map["room_category"] as String))
-                        }.addOnFailureListener{
-                            Log.e("firebase", "Error getting data", it)
-                        }
-                    }
-                }
-                this.notifyDataSetChanged()
-                Log.d("msg", "들림")
-            }.addOnFailureListener{
-                Log.e("firebase", "Error getting data", it)
-            }
-        }
+    inner class HomeRecyclerViewAdapter(val room_data_list : ArrayList<room_Data>) : RecyclerView.Adapter<HomeRecyclerViewHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeRecyclerViewHolder {
             // 0, 1, 2 parameter -> 0 : view로 팽창할 것, 1 : 팽창해서 recyclerView(parent)에 표시, 2 : false
